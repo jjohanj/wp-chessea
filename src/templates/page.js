954327b092f88493bed34a9fc0773cdabcbcf2ed
jjohanj <johanjorritsma@hotmail.com>
2019-08-17 23:12:15 +0200

    grid css start

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,13 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
+import Image from "../components/image"
+import 'page.css';
 
 
 function Page({ pageContext }) {
 
 return (
   <Layout>
+  <div className="grid-container">
+  <div className="grid-text">
   <h1>{pageContext.title}</h1>
    <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
+   <div>
+   <div className="grid-img"></Image></di>
+   </div>
   </Layout>
 )
 }
