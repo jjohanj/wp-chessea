17bdd80eaee315bf5164477963405ac1ccf03f1d
jjohanj <johanjorritsma@hotmail.com>
2019-08-18 00:07:02 +0200

    typo

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
-import 'page.css';
+import './page.css';
 
 
 function Page({ pageContext }) {
