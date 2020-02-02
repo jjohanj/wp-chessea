1940eb451ac319ff3544170ded98b2b964a7cdcd
jjohanj <johanjorritsma@hotmail.com>
2020-02-02 11:32:45 +0100

    serviceworker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,11 +10,5 @@ import "typeface-raleway";
 import "typeface-catamaran";
 
 export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-    `Reload to display the latest version?`
-  )
-  if (answer === true) {
     window.location.reload()
   }
-}
