b86f763277d773731b41c4588f34f94b01d36d00
jjohanj <johanjorritsma@hotmail.com>
2020-03-10 14:01:44 +0100

    fancybox

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -1,12 +1,17 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Image from "../components/image"
 import MainMenu from "../components/mainmenu"
 import {graphql, useStaticQuery } from "gatsby"
 import SEO from '../components/seo';
 import Footer from "../components/footer"
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
 
 const Gallery = ({}) => {
+  const [photoIndex, setphotoIndex] = useState(0);
+
+  const [isOpen, setisOpen] = useState(false);
 
   const data = useStaticQuery(graphql`
     query {
@@ -16,6 +21,7 @@ const Gallery = ({}) => {
             title
             localFile {
               base
+              url
             }
           }
         }
@@ -23,10 +29,11 @@ const Gallery = ({}) => {
     }
   `);
 
-
+let images = data.allWordpressWpMedia.edges;
+console.log(images[1].node.localFile.url);
     let counter;
     counter = -1;
-  const post =  data.allWordpressWpMedia.edges.map((item) => {
+  const post =  data.allWordpressWpMedia.edges.map((item, index) => {
       if (item.node.title === "board") {
           return null
       }
@@ -34,12 +41,12 @@ const Gallery = ({}) => {
       switch (counter) {
           case 0:
               return <div key={counter} className={`img-xl-${counter}`}>
-                  <div className="hero"><Image imgName={item.node.localFile.base}/></div>
+                  <div className="hero pointer" onClick={(event) => { setphotoIndex(index); setisOpen(true);}}><Image imgName={item.node.localFile.base}/></div>
               </div>;
           case 1:
           case 2:
               return <div key={counter} className={`img-lg-${counter}`}>
-                  <div className="hero"><Image imgName={item.node.localFile.base}/></div>
+                  <div className="hero pointer" onClick={(event) => { setphotoIndex(index); setisOpen(true);}}><Image imgName={item.node.localFile.base}/></div>
               </div>;
           case 3:
           case 4:
@@ -49,12 +56,12 @@ const Gallery = ({}) => {
           case 8:
               counter++;
               return <div key={counter} className={`img-sm-${counter}`}>
-                  <div className="hero"><Image imgName={item.node.localFile.base}/></div>
+                  <div className="hero pointer" onClick={(event) => { setphotoIndex(index); setisOpen(true);}}><Image imgName={item.node.localFile.base}/></div>
               </div>;
           case 9:
           default:
               return <div key={counter} className="img-gallery">
-                  <div className="hero"><Image imgName={item.node.localFile.base}/></div>
+                  <div className="hero pointer" onClick={(event) => { setphotoIndex(index); setisOpen(true);}}><Image imgName={item.node.localFile.base}/></div>
               </div>
       }
   });
@@ -66,6 +73,20 @@ const Gallery = ({}) => {
       <div className="gallery">
         {post}
       </div>
+{isOpen && (
+   <Lightbox
+     mainSrc={images[photoIndex].node.localFile.url}
+     nextSrc={images[(photoIndex + 1) % images.length]}
+     prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+     onCloseRequest={() => setisOpen(false)}
+     onMovePrevRequest={() =>
+       setphotoIndex((photoIndex + images.length - 1) % images.length)
+     }
+     onMoveNextRequest={() =>
+       setphotoIndex ((photoIndex + 1) % images.length)
+     }
+   />
+ )}
       <Footer />
       </div>
     </>
