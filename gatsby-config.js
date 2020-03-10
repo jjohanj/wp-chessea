7920032309ea56f98e04d22c4c36b78fc45547e0
jjohanj <johanjorritsma@hotmail.com>
2020-03-10 14:17:03 +0100

    icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -139,7 +139,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/butterfly.jpg`,
+        icon: `src/images/butterfly.png`,
       },
     },
     'gatsby-plugin-offline',
