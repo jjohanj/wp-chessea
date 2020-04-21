a57b0a735950e66237614085cf9e0cbfdb17c9db
jjohanj <johanjorritsma@hotmail.com>
2020-04-21 11:17:27 +0200

    periode 4

--- src/components/paardensprong.js
+++ src/components/paardensprong.js
@@ -7,13 +7,13 @@ const Paardensprong = () => {
     <>
     <div className="igrid">
       <div className="koning iframe-container">
-        <iframe src="https://brambaptist.nl/competitie/2019-2020/koning/p3/" height="500" width="450"></iframe>
+        <iframe src="https://brambaptist.nl/competitie/2019-2020/koning/p4/" height="500" width="450"></iframe>
       </div>
       <div className="dame iframe-container">
-        <iframe src="https://brambaptist.nl/competitie/2019-2020/dame/p3/" height="500" width="450"></iframe>
+        <iframe src="https://brambaptist.nl/competitie/2019-2020/dame/p4/" height="500" width="450"></iframe>
       </div>
       <div className="iframe-container toren">
-        <iframe src="https://brambaptist.nl/competitie/2019-2020/toren/p3/" height="500" width="450"></iframe>
+        <iframe src="https://brambaptist.nl/competitie/2019-2020/toren/p4/" height="500" width="450"></iframe>
       </div>
     </div>
     </>
