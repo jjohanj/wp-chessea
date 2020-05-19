8e2439941306b540d0a561042fcf97278d78a5a5
jjohanj <johanjorritsma@hotmail.com>
2020-05-19 10:32:29 +0200

    css

--- src/components/footer.js
+++ src/components/footer.js
@@ -37,16 +37,16 @@ function Footer() {
     <div className="contact">
       <div className="social">
         <h5>Contact:</h5>
-        <ul className="list-inline">
-          <li className="list-inline-item"><a href="mailto:johanjorritsma@hotmail.com"><FaEnvelopeSquare />E-mail</a></li>
-          <li className="list-inline-item"><a  href="https://nl.linkedin.com/in/johan-jorritsma" target="_blank" rel="noopener noreferrer"><FaLinkedin />Linkedin</a></li>
+        <ul>
+          <li><a href="mailto:johanjorritsma@hotmail.com"><FaEnvelopeSquare />E-mail</a></li>
+          <li><a  href="https://nl.linkedin.com/in/johan-jorritsma" target="_blank" rel="noopener noreferrer"><FaLinkedin />Linkedin</a></li>
         </ul>
       </div>
       <div className="clubs">
         <h5>Clubs:</h5>
-        <ul className="list-inline">
-          <li className="list-inline-item"><a  href="https://www.scdepaardensprong.nl" target="_blank" rel="noopener noreferrer"><FaChess />SC de Paardensprong</a></li>
-          <li className="list-inline-item"><a  href="https://www.groningercombinatie.nl/" target="_blank" rel="noopener noreferrer"><FaChess />Groninger Combinatie</a></li>
+        <ul>
+          <li><a  href="https://www.scdepaardensprong.nl" target="_blank" rel="noopener noreferrer"><FaChess />SC de Paardensprong</a></li>
+          <li><a  href="https://www.groningercombinatie.nl/" target="_blank" rel="noopener noreferrer"><FaChess />Groninger Combinatie</a></li>
         </ul>
         </div>
         </div>
