2e272761a0665cfaba64ea1aca10757523b10edd
jjohanj <johanjorritsma@hotmail.com>
2020-06-02 00:08:57 +0200

    svg

--- src/components/image.js
+++ src/components/image.js
@@ -39,7 +39,7 @@ const Image = ({ imgName }) => (
           edges {
             node {
               fluid(maxWidth: 650) {
-                ...GatsbyImageSharpFluid_withWebp_noBase64
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
                 originalName
               }
             }
