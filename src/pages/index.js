3eaef3f76fed091195112b42fa4fda166ac6caf6
jjohanj <johanjorritsma@hotmail.com>
2020-06-02 15:13:08 +0200

    forget articles

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
   return (
     <Layout aPage="grid-container home">
         <Articles liststart ="0"  listend="4" type="main"/>
-
+        <ArticlesSecond liststart ="4"  listend="8" type="secondary"/>
     </Layout>
   )
 }
