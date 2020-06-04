35ad11198d59d93fabd513758d18ab976a9862f0
jjohanj <johanjorritsma@hotmail.com>
2020-06-04 23:36:08 +0200

    react lazy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React, { lazy, Suspense } from 'react';
 import Articles from "../components/articles"
 import Layout from "../components/layout"
-import ArticlesSecond from "../components/articles-second"
 
+const ArticlesSecond = lazy(() => import('../components/articles-second'));
+
+const renderLoader = () => <p>Loading</p>;
 const IndexPage = () => {
 
   return (
     <Layout aPage="grid-container home">
         <Articles liststart ="0"  listend="4" type="main"/>
+        <Suspense fallback={renderLoader()}>
         <ArticlesSecond liststart ="4"  listend="8" type="secondary"/>
+        </Suspense>
     </Layout>
   )
 }
