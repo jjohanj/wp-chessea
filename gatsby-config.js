90f692554128b18e323a00e12913287aaa787a19
jjohanj <johanjorritsma@hotmail.com>
2020-06-04 10:07:29 +0200

    fix for preload error

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Johan`,
   },
   plugins: [
+    'gatsby-plugin-preload-link-crossorigin',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
