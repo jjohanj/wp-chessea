c81ca6090d8f6a7d6c2433f1aebd7dd614196afd
jjohanj <johanjorritsma@hotmail.com>
2020-06-04 23:58:30 +0200

    loadable components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `Johan`,
   },
   plugins: [
+    'gatsby-plugin-loadable-components-ssr',
     'gatsby-plugin-preload-link-crossorigin',
     `gatsby-plugin-react-helmet`,
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
-import React, { lazy, Suspense } from 'react';
+import React from "react"
 import Articles from "../components/articles"
 import Layout from "../components/layout"
+import loadable from '@loadable/component'
 
-const ArticlesSecond = lazy(() => import('../components/articles-second'));
+const ArticlesSecond = loadable(() => import('../components/articles-second'))
 
-const renderLoader = () => <p>Loading</p>;
 const IndexPage = () => {
 
   return (
     <Layout aPage="grid-container home">
         <Articles liststart ="0"  listend="4" type="main"/>
-        <Suspense fallback={renderLoader()}>
         <ArticlesSecond liststart ="4"  listend="8" type="secondary"/>
-        </Suspense>
     </Layout>
   )
 }
