dcb06313e017f5b103b1e31f25e63dd4649d9ddb
jjohanj <johanjorritsma@hotmail.com>
2020-06-06 10:16:26 +0200

    back to before palleton

--- src/components/articles.js
+++ src/components/articles.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Image from "../components/image"
 import {graphql, useStaticQuery } from "gatsby"
 import Parser from 'html-react-parser'
-import {Palette} from 'react-palette';
+
 
 const Articles = (props) => {
 
@@ -26,12 +26,6 @@ const Articles = (props) => {
               featured_media {
                 localFile {
                   base
-                    childImageSharp {
-              fluid {
-                srcWebp
-              }
-            }
-
                 }
               }
               tags {
@@ -49,15 +43,14 @@ const Articles = (props) => {
     };
 
     const post =  data.allWordpressPost.edges.slice(props.liststart, props.listend).map((item, i) => {
-       console.log(item.node.featured_media.localFile.base)
       return (
         <article className={`${props.type}-articles ${props.type}-article-${i}`} key={i}  onClick={() => handleClick(props.type, i)} >
-
+          <Image imgName={item.node.featured_media.localFile.base}/>
           <div className="content">
-                    <h3 ><Link  to={`/articles/${item.node.slug}`}>{Parser(item.node.title)}</Link></h3>
+                    <h3><Link to={`/articles/${item.node.slug}`}>{Parser(item.node.title)}</Link></h3>
                     <p className="date"><span>{item.node.acf.datum}</span></p>
-                    {Parser(item.node.content.substring(0,130) + ' ...' )}
-                      </div>
+                    <div>{Parser(item.node.content.substring(0,130) + ' ...' )} </div>
+          </div>
         </article>
       )});
 
