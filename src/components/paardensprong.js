9d1cf45266c9f568c77938b73140d9e594f03832
Johan <johan@devblue.nl>
2020-11-28 14:13:06 +0100

    competitie 2

--- src/components/paardensprong.js
+++ src/components/paardensprong.js
@@ -7,13 +7,13 @@ const Paardensprong = () => {
     <>
     <div className="igrid">
       <div className="koning iframe-container">
-        <iframe src="https://brambaptist.nl/competitie/2020-2021/koning/p1/" height="500" width="450"></iframe>
+        <iframe src="https://brambaptist.nl/competitie/2020-2021/koning/p2/" height="500" width="450"></iframe>
       </div>
       <div className="dame iframe-container">
-        <iframe src="https://brambaptist.nl/competitie/2020-2021/dame/p1/" height="500" width="450"></iframe>
+        <iframe src="https://brambaptist.nl/competitie/2020-2021/dame/p2/" height="500" width="450"></iframe>
       </div>
       <div className="iframe-container toren">
-        <iframe src="https://brambaptist.nl/competitie/2020-2021/toren/p1/" height="500" width="450"></iframe>
+        <iframe src="https://brambaptist.nl/competitie/2020-2021/toren/p2/" height="500" width="450"></iframe>
       </div>
     </div>
     </>
