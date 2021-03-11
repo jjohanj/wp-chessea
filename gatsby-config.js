ea3d38277e6f8e65d626d92d0efe8f225e119d08
Johan <johanjorritsma@hotmail.com>
2021-03-11 23:01:55 +0100

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Chessea`,
+        short_name: `Chessea`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
       resolve: "gatsby-source-wordpress",
       options: {
         url: "https://flonxchess.nl/graphql",
@@ -14,6 +23,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    'gatsby-plugin-offline',
     {
       resolve: "gatsby-source-filesystem",
       options: {
